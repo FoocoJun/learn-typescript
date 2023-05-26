7d64c0389af03486deb893b141b68622ff0854fb
Hajun Song <hjsong@barogo.com>
2023-05-26 11:20:46 +0900

    섹션 1 완강!

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,40 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} city
+ * @property {string} street
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name 
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @typedef {object} UserData
+ * @property {User} data 
+ */
+
+/**
+ * 
+ * @returns {Promise<UserData>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
-      user = response.data;
+      user= response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
---
+++ why-ts/sample.ts
@@ -0,0 +1,6 @@
+function sum(a:number, b:number) {
+    return a + b;
+}
+
+var result = sum(10, 20);
+var string = result.toLocaleString();
